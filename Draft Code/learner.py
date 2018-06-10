768c235dcd44d72ea24e1eac9f16a74c4462ac0c
anotherk1nd <jfenech22@hotmail.com>
2018-06-10 17:06:54 +0200

    Saving full mfcc to csv, trying to find more efficient way to save and load, about to implement pandas

--- Draft Code/learner.py
+++ Draft Code/learner.py
@@ -2,14 +2,15 @@ from preprocessing import *
 from mfcc_class_method import *
 
 class learner(preprocessing, audio):
-    pass
-    # def __init__(self,subsfn,mp3,sr,duration):
-    #     learner.__init__(self,subsfn)
-    #     learner.__init__(self, mp3,sr,duration)
+    #pass
+    def __init__(self,subsfn,mp3,sr,duration):
+        preprocessing.__init__(self,subsfn)
+        audio.__init__(self,mp3,sr,duration)
 
 
-a = learner('gotS07E01.srt')
-a.open_subs()
-a.pb_array_init()
-a.pb_array_fill()
-print(a.pb_array)
+# a = learner('gotS07E01.srt','gotS07E01_16k.mp3',16000,5)
+# a.open_subs()
+# a.pb_array_init()
+# a.pb_array_fill()
+# a.mfcc()
+# print(a.mfccs)
--- Draft Code/mfcc_class_method.py
+++ Draft Code/mfcc_class_method.py
@@ -3,29 +3,36 @@ from python_speech_features import mfcc
 import pandas as pd
 import scipy as sp
 import numpy as np
-
-class MyAppLookupError(LookupError):
-    '''raise this when there's a lookup error for my app'''
+import time
+import timeit
 
 class audio:
     #Here we implement the audio preprocessing, with the aim of possibly integrating this with the peprocessing method.
 
     def __init__(self,mp3,sr,duration):
         self.duration = duration
+        t0 = time.time()
         self.audio, self.sr = lsa.load(mp3, sr=sr,duration=self.duration)
+        t00 = time.time()
+        self.time0 = t00 - t0
         self.df = pd.DataFrame({'data': self.audio})
         self.mfccs = []
-
+        self.time = 0.0
+        #self.mfcc_fn = ""
 
     def mfcc(self):
         try:
+            t1 = time.time()
             self.mfccs = mfcc(self.df['data'],samplerate=self.sr,nfft=512)
+            t2 = time.time()
+            self.time = t2 - t1
+
             if np.isnan(np.sum(self.mfccs)): #This returns boolean value describing whether nan is present
                 raise ValueError
         except ValueError:
             print("WARNING: nan values present, check these only occur at beginning and end of file, if so continue to replace with float zeros")
             print("Length of mfcc array: ",len(self.mfccs))
-            print("nan indices: ",np.where(np.isnan(self.mfccs)))
+            print("nan indices: ",np.where(np.isnan(self.mfccs))) #need to clean up the output to only print row numbers i think but may miss non nan entries if i do
             var = input("Are nan's only at beginning and end? y/n")
             if var == "y":
                 self.mfccs[np.where(np.isnan(self.mfccs))] = float(0.0)
@@ -33,10 +40,30 @@ class audio:
             else:
                 print("Process terminated")
 
+    def save_mfcc(self):
+        sp.savetxt('mfcc.csv',self.mfccs, delimiter=',')
 
-fun = audio('gotS07E01_16k.mp3',16000,5)
-fun.mfcc()
-a = fun.mfccs
+    def load_mfcc(self,mfcc_fn):
+        self.mfccs = sp.loadtxt(mfcc_fn,delimiter=',')
+
+
+fun = audio('gotS07E01_16k.mp3',16000,3587)
+#fun.mfcc()
+#a = fun.mfccs
+#print(a)
+#fun.save_mfcc()
+#print(fun.time0,fun.time)
+time1 = time.time()
+fun.load_mfcc('mfcc.csv')
+time11 = time.time()
+#print("load mp3 time", fun.time0)
+#print("mfcc time", fun.time)
+print("load mfcc time",time11-time1)
+#b = fun.mfccs
+#print(sp.array_equal(a,b)) #returns true
+# print(a)
+# print(b)
+#print(timeit.timeit(fun.mfcc))
 # print(a[0])
 # print(a[1])
 # print(np.isnan(np.sum(a))) #This returns boolean value describing whether nan is present
